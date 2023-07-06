62a0b400b60f4f68ef2953938704600e482a9738
Vishal Sharma <vishaliitr7@gmail.com>
2023-07-06 16:45:53 +0530

    dimensions

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from ukcensus.CensusData import RateLimitedAPI
 if __name__ == "__main__":
     api = RateLimitedAPI()
 
-    population_types = api.get_population_types()
+    # population_types = api.get_population_types()
 
-    print(population_types)
-    print(api.get_area_types("unused-pop-type-label"))
+    # print(population_types)
+    print(api.get_area_types())
+    print(api.get_area_infos())
+    print(api.get_dimensions("religion"))
     # print(api.get_area_types("All usual residents"))
--- ukcensus/CensusData.py
+++ ukcensus/CensusData.py
@@ -31,7 +31,7 @@ class RateLimitedAPI:
         self.db_user = config.get('DB', 'username')
         self.db_password = config.get('DB', 'password')
 
-    def make_request(self, endpoint, return_type="json"):
+    def make_request(self, endpoint, params={}):
         elapsed_time = time.time() - self.start_time
         if elapsed_time < 10 and self.requests_made >= 120:
             wait_time = 10 - elapsed_time
@@ -44,52 +44,47 @@ class RateLimitedAPI:
             self.reset_timer()
 
         url = f"{self.base_url}/{endpoint}"
-        response = requests.get(url)
+        response = requests.get(url, params=params)
         self.requests_made += 1
         result = response.json()
 
         if response.status_code == 200:
-            if return_type == "df":
-                try:
-                    return pd.DataFrame(result["items"])
-                except KeyError:
-                    return pd.DataFrame(result)
-            if return_type == "pl_df":
-                try:
-                    response = pd.DataFrame(result["items"])
-                    return pl.from_pandas(response)
-                except KeyError:
-                    if return_type == "pl_df":
-                        return pl.from_dict(result)
-            else:
-                try:
-                    return result["items"]
-                except KeyError:
-                    return result
+            return result
         else:
             raise Exception(f"Request failed with status code {response.status_code}")
+    
+    def fetch_all_data(self, endpoint, return_type="json", p={}):
+        limit = 100
+        offset = 0
+        total_count = -1
+
+        results = []
+
+        while total_count == -1 or offset < total_count:
+            params = {
+                "limit": limit,
+                "offset": offset,
+                **p
+            }
+            response = self.make_request(endpoint, params=params)
+            if total_count == -1:
+                total_count = response["total_count"]
+            
+            if response["items"] is not None:
+                results.extend(response["items"])
+            
+            offset += limit
+
+            print(f"Done Fetching {endpoint} {offset}/{total_count}")
+
+            # TODO: Remove this
+            if offset > limit:
+                break
+
         
-
-        total_count = response["total_count"]
-    # # Set the limit and offset values for pagination
-    # limit = 500
-    # offset = 0
-
-    # # Fetch the remaining data sequentially using pagination
-    # while offset < total_count:
-    #     endpoint = "population-types"
-    #     params = {
-    #         "limit": limit,
-    #         "offset": offset
-    #     }
-    #     response = self.make_request(endpoint, params)
-    #     self.create_table_if_not_exists(tablename)
-    #     self.add_to_database(tablename, response)
-
-    #     # Increment the offset for the next page
-    #     offset += limit
-
-    # return response
+        if return_type == "df":
+            return pd.DataFrame(results)
+        return results
 
     def reset_timer(self):
         self.start_time = time.time()
@@ -140,26 +135,25 @@ class RateLimitedAPI:
         return pd.DataFrame(data=results, columns=[x[0] for x in cursor.description])
 
     def get_population_types(self, return_type="json"):
-        endpoint = "population-types?limit=100"
+        endpoint = "population-types"
         self.create_table_if_not_exists("population-types")
-        response = self.make_request(endpoint, return_type)
+        response = self.fetch_all_data(endpoint, return_type)
 
         for item in response: 
             self.add_to_database("population-types", item)
 
         return response
 
-    def get_area_types(self, population_type, return_type="json"):
-        
+    def get_area_types(self, return_type="json"):
         areas_query = """
-        SELECT data->>'name' as name FROM "area-types" where data->>'label' = '{}'
-        """.format("All usual residents")
+        SELECT data->>'id' as id FROM "area-types"
+        """
         try:
             response = self.get_results_from_database(areas_query)
 
         except UndefinedTable:
             endpoint = "area-types"
-            self.create_table_if_not_exists(endpoint)
+            self.create_table_if_not_exists("area-types")
 
             select_query = """
                 SELECT data->>'name' as name FROM "population-types" where data->>'label' = '{}'
@@ -168,29 +162,77 @@ class RateLimitedAPI:
             response = self.get_results_from_database(select_query)
 
             self.create_table_if_not_exists("area-types")
+            
             for _,name in response['name'].items():
                 endpoint = 'population-types/{population_type}/area-types'.format(population_type=name)
-                response = self.make_request(endpoint, return_type)
+                response = self.fetch_all_data(endpoint, return_type)
                 response = [dict(item, **{'population-type':name}) for item in response]
                 for item in response:
                     self.add_to_database("area-types", item)
             
             response = self.get_results_from_database(areas_query)
+        
         return response
+    
 
-            # add the population_type related 
-            # area_types.append(area_types)
-        # for item in response["name"]:
-        #     endpoint = "population-types/{population_type}/area-types/{area_type}/areas".format(population_type=population_type, area_type=item)
-        #     response = self.make_request(endpoint, return_type)
+    def get_area_infos(self, return_type="json"):
+        areas_query = """
+        SELECT data->>'id' as id FROM "area-infos"
+        """
+        try:
+            response = self.get_results_from_database(areas_query)
 
-        #     self.add_to_database(endpoint, response)
+        except UndefinedTable:
+            endpoint = "area-infos"
+            self.create_table_if_not_exists("area-infos")
 
-        # for item in response["data"]:
+            select_query = """
+                SELECT data->>'id' as id, data ->>'population-type' as population FROM "area-types"
+                """
 
-        #     endpoint = 'population-types/{population_type}/area-types/lsoa/areas'.format(population_type=population_type)
-        #     response = self.make_request(endpoint, return_type)
+            response = self.get_results_from_database(select_query)
 
-        #     self.add_to_database(endpoint, response)
+            self.create_table_if_not_exists("area-infos")
+            
+            for _,row in response.iterrows():
+                endpoint = 'population-types/{population_type}/area-types/{area_type}/areas'.format(population_type=row.population, area_type=row.id)
+                response = self.fetch_all_data(endpoint, return_type)
+                for item in response:
+                    self.add_to_database("area-infos", item)
+            
+            response = self.get_results_from_database(areas_query)
+        
+        return response
+    
 
-        # return response
+    def get_dimensions(self, q_param, return_type="json"):
+        dimension_query = """
+        SELECT data->>'id' as id FROM "dimensions"
+        """
+        try:
+            response = self.get_results_from_database(dimension_query)
+
+        except UndefinedTable:
+            endpoint = "dimensions"
+            self.create_table_if_not_exists("dimensions")
+
+            select_query = """
+                SELECT data->>'name' as name FROM "population-types" where data->>'label' = '{}'
+                """.format("All usual residents")
+
+            response = self.get_results_from_database(select_query)
+
+            self.create_table_if_not_exists("dimensions")
+            
+            for _,name in response['name'].items():
+                endpoint = 'population-types/{population_type}/dimensions'.format(population_type=name)
+                response = self.fetch_all_data(endpoint, return_type, p={"q": q_param})
+                response = [dict(item, **{'population-type':name}) for item in response]
+                for item in response:
+                    self.add_to_database("dimensions", item)
+            
+            response = self.get_results_from_database(dimension_query)
+        
+        return response
+    
+
